2b0ab8886e8560f0c4d27182f5373a8525194481
shumayel.khan@mail.utoronto.ca <shumayel.khan@mail.utoronto.ca>
2015-06-16 16:29:07 -0400

    Added Add a Contact, \/Contacts page is working correctly, Time added to Index

--- crm.rb
+++ crm.rb
@@ -2,5 +2,12 @@ require 'sinatra'
 
 get '/' do
 @crm_app_name = "My CRM"
+@time = Time.new
   erb :index
+end
+
+get '/contacts' do
+@contacts_page = "Rolodex"
+@new_contact = "Add a New Contact"
+    erb :contacts
 end
