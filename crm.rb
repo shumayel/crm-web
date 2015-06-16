b721dbbf0bdf8c27849980478476d5d79dceba9c
shumayel.khan@mail.utoronto.ca <shumayel.khan@mail.utoronto.ca>
2015-06-16 17:21:06 -0400

    Removed default contacts, added Rolodex.rb, can add actual Contacts now

--- crm.rb
+++ crm.rb
@@ -2,6 +2,8 @@ require_relative 'rolodex.rb'
 require_relative 'contact.rb'
 require 'sinatra'
 
+$rolodex = Rolodex.new
+
 get '/' do
 @crm_app_name = "My CRM"
 @time = Time.new
@@ -11,9 +13,9 @@ end
 get '/contacts' do
 
  @contacts = []
-  @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
-  @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
-  @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
+  # @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
+  # @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
+  # @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
 
 @contacts_page = "Rolodex"
 @new_contact = "Add a New Contact"
