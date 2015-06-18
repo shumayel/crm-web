2346f7209a709988f1325c0b7b2528f435b3768e
shumayel.khan@mail.utoronto.ca <shumayel.khan@mail.utoronto.ca>
2015-06-18 16:58:39 -0400

    Added raise Sinatra::NotFound

--- crm.rb
+++ crm.rb
@@ -3,9 +3,11 @@ require_relative 'contact.rb'
 require 'sinatra'
 
 $rolodex = Rolodex.new
-@@rolodex = Rolodex.new
-
-@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar Programmer"))
+$rolodex = Rolodex.new
+$rolodex.add_contact(Contact.new(1, "Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar Programmer"))
+$rolodex.add_contact(Contact.new(2, "Powerpuff", "Girl", "powerpuff@bitmakerlabs.com", "Powerpuff Girls"))
+$rolodex.add_contact(Contact.new(3, "Dexters", "Lab", "dexter@bitmakerlabs.com", "Dexter Labs"))
+$rolodex.add_contact(Contact.new(4, "Meemee", "Dexter", "meemee@bitmakerlabs.com", "Dexters Sister"))
 
 get '/' do
 @crm_app_name = "The Boutique Rolodex."
@@ -32,12 +34,16 @@ end
 
 post '/contacts' do
     puts params
-  new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+  new_contact = Contact.new(param [:id], params[:first_name], params[:last_name], params[:email], params[:note])
   $rolodex.add_contact(new_contact)
   redirect to('/contacts')
 end
 
-get "/contacts/1" do
-@contact = @@rolodex.find_contact(1)
-erb :show_contact
+get "/contacts/:id" do
+    @contact = $rolodex.find_contact(params[:id].to_i)
+    if @contact
+        erb :show_contact
+else
+    raise Sinatra::NotFound
+    end
 end
