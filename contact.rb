3fc857f2e81c35fd177cadbdec0896dac9ab3535
shumayel.khan@mail.utoronto.ca <shumayel.khan@mail.utoronto.ca>
2015-06-18 16:27:25 -0400

    First commit as per Part 3 of assignment, just created show_contact.erb

--- contact.rb
+++ contact.rb
@@ -2,8 +2,8 @@ class Contact
   attr_accessor :first_name, :last_name, :email, :notes
   attr_reader :id
 
-  def initialize(first_name, last_name, email, notes)
-    # @id = id
+  def initialize(id, first_name, last_name, email, notes)
+    @id = id
     @first_name = first_name
     @last_name = last_name
     @email = email
--- crm.rb
+++ crm.rb
@@ -3,6 +3,9 @@ require_relative 'contact.rb'
 require 'sinatra'
 
 $rolodex = Rolodex.new
+@@rolodex = Rolodex.new
+
+@@rolodex.add_contact(Contact.new("Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar Programmer"))
 
 get '/' do
 @crm_app_name = "The Boutique Rolodex."
@@ -32,4 +35,9 @@ post '/contacts' do
   new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
   $rolodex.add_contact(new_contact)
   redirect to('/contacts')
+end
+
+get "/contacts/1" do
+@contact = @@rolodex.find_contact(1)
+erb :show_contact
 end
