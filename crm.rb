87bdcaeb4fc7829f159f5fd4b61e3deca66846ef
shumayel.khan@mail.utoronto.ca <shumayel.khan@mail.utoronto.ca>
2015-06-19 15:25:34 -0400

    Figured out why contacts were not showing up on contacts page. Change @@rolodex left overs to Contact.datamappermethods, have to update form still I believe

--- crm.rb
+++ crm.rb
@@ -1,4 +1,4 @@
-require_relative 'rolodex.rb'
+# require_relative 'rolodex.rb'
 require 'sinatra'
 require 'data_mapper'
 
@@ -7,34 +7,37 @@ DataMapper.setup(:default, "sqlite3:database.sqlite3")
 class Contact
     include DataMapper::Resource
 
-      property :id, Serial
-      property :first_name, String
-      property :last_name, String
-      property :email, String
-      property :notes, String
+    property :id, Serial
+    property :first_name, String
+    property :last_name, String
+    property :email, String
+    property :notes, String
+
+
 end
+
 
 DataMapper.finalize
 DataMapper.auto_upgrade!
 
-$rolodex = Rolodex.new
-$rolodex = Rolodex.new
+# $rolodex = Rolodex.new
+# $rolodex = Rolodex.new
 # $rolodex.add_contact(Contact.new(1, "Johnny", "Bravo", "johnny@bitmakerlabs.com", "Rockstar")
 
-get '/' do
-    @crm_app_name = "The Boutique Rolodex."
-    @boutique = "The boutique rolodex."
-    @time = Time.new
-    erb :index
-end
+    get '/' do
+        @crm_app_name = "The Boutique Rolodex."
+        @boutique = "The boutique rolodex."
+        @time = Time.new
+        erb :index
+    end
 
 
-get '/contacts' do
-    @contacts = Contact.all
+    get '/contacts' do
+        @contacts = Contact.all
   # @contacts << Contact.new("Yehuda", "Katz", "yehuda@example.com", "Developer")
   # @contacts << Contact.new("Mark", "Zuckerberg", "mark@facebook.com", "CEO")
   # @contacts << Contact.new("Sergey", "Brin", "sergey@google.com", "Co-Founder")
- @contacts_page = "Rolodex"
+  @contacts_page = "Rolodex"
   @new_contact = "Add a New Contact"
   erb :contacts
 end
@@ -45,12 +48,12 @@ end
 
 post '/contacts' do
     contact = Contact.create(
-        :id => params[:id],
         :first_name => params[:first_name],
         :last_name => params[:last_name],
         :email => params[:email],
         :notes => params[:notes]
         )
+
     redirect to ('/contacts')
 end
 
@@ -63,23 +66,23 @@ get "/contacts/:id" do
     end
 end
 
-get "/contacts/:id/edit" do
-   @contact = $rolodex.find_contact(params[:id].to_i)
-   if @contact
+put "/contacts/:id/edit" do
+ @contact = Contact.update(params[:id].to_i)
+ if @contact
     erb :edit_contact
-    else
+else
     raise Sinatra::NotFound
-    end
+end
 end
 
 delete "/contacts/:id" do
-  @contact = $rolodex.find_contact(params[:id].to_i)
-    if @contact
-    $rolodex.delete_contact(@contact)
+  @contact = Contact.destroy(params[:id].to_i)
+  if @contact
+    @contact.delete_contact(@contact)
     redirect to("/contacts")
-    else
-        raise Sinatra::NotFound
-    end
+else
+    raise Sinatra::NotFound
+end
 end
 
 # get "/contacts/1" do
